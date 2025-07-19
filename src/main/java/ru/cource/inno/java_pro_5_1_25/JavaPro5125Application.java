5b14775c698cfe288f84bca2ca3548eb1eff97c9
Люсточкин Кирилл Сергеевич <lustochkin@dev.vtb.ru>
2025-07-19 12:16:30 +0300

    init

---
+++ src/main/java/ru/cource/inno/java_pro_5_1_25/JavaPro5125Application.java
@@ -0,0 +1,13 @@
+package ru.cource.inno.java_pro_5_1_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPro5125Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPro5125Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cource/inno/java_pro_5_1_25/JavaPro5125ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cource.inno.java_pro_5_1_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPro5125ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
